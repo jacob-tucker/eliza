4d45d4b8dd7f0ce6fe737f56ffc835f1b6b84525
jacob-tucker <jacobtucker818@gmail.com>
2025-01-29 17:24:35 -0800

    ensure no duplicates, plus new flux finetuned model

--- packages/client-twitter/src/functions/generate/generate.ts
+++ packages/client-twitter/src/functions/generate/generate.ts
@@ -11,7 +11,7 @@ const IMAGE_PROMPT_INPUT = `imagine a blockchain feature that seems obvious but 
 it should make everyday apps feel completely different when you add tokenized elements or onchain logic.
 keep it short, lowercased, and broken into small paragraphs.
 speak casually, like you just realized something big that flips the usual approach on its head.
-some ideas include an improvement to a crypto wallet, new ways to own things on the internet, new ways to port your onchain data across different apps, better ways to swap or spend funds, new ways to tokenize things, or any general way blockchain can make everyday apps better. 
+some ideas include an improvement to a crypto wallet, new ways to own things on the internet, new ways to port your onchain data across different apps, better ways to swap or spend funds, new ways to tokenize things, or any general way blockchain can make everyday apps better.
 here are some example outputs, which should be very similar to what you output. They include one sentence to highlight the problem, and one sentence to communicate how the design solves the problem. Note that I only want you to output one, not multiple options:
 1. betting on sports is fun, but it’s even more fun if you could do it with your friends.\n\nwhat if you could use smart contracts to bet with your friends in the group chat?
 2. rental applications are a pain and involve sharing sensitive personal information over email.\n\n what if you could prove you meet the requirements of a rent application without sharing any personal information with zero knowledge proofs?
@@ -34,7 +34,9 @@ export async function generate(): Promise<string> {
 
     // Additionally, you have context about current events to make your text timely and relevant. You can use these events in your ideation to inform your design mockup if you want to, use your judgement to decide whether you want to use it or not: ${perplexityContext}`;
 
-    messages.push({ role: "user", content: IMAGE_PROMPT_INPUT });
+    if (messages[messages.length - 1].role !== "user") {
+        messages.push({ role: "user", content: IMAGE_PROMPT_INPUT });
+    }
 
     const openai = new OpenAI({
         baseURL: "https://api.deepseek.com",
@@ -48,7 +50,6 @@ export async function generate(): Promise<string> {
             model: "deepseek-reasoner",
         });
         const content = completion.choices[0].message.content;
-        console.log("deepseek content", content);
         messages.push({ role: "assistant", content: content });
         return content;
     } catch (error) {
